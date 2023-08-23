fb9ac7decd93c447bcbf4ed9a8ce9a34fe18ce11
Thomas Loimer <thomas.loimer@tuwien.ac.at>
2023-08-23 23:52:46 +0200

    Whitespace changes

--- src/u_free.c
+++ src/u_free.c
@@ -33,214 +33,228 @@
 #include "w_drawprim.h"
 
 
-void free_arc(F_arc **list)
+void
+free_arc(F_arc **list)
 {
-    F_arc	   *a, *arc;
+	F_arc	   *a, *arc;
 
-    for (a = *list; a != NULL;) {
-	arc = a;
-	a = a->next;
-	if (arc->for_arrow)
-	    free((char *) arc->for_arrow);
-	if (arc->back_arrow)
-	    free((char *) arc->back_arrow);
-	if (arc->comments)
-	    free(arc->comments);
-	free((char *) arc);
-    }
-    *list = NULL;
-}
-
-void free_compound(F_compound **list)
-{
-    F_compound	   *c, *compound;
-
-    for (c = *list; c != NULL;) {
-	compound = c;
-	c = c->next;
-	free_arc(&compound->arcs);
-	free_compound(&compound->compounds);
-	free_ellipse(&compound->ellipses);
-	free_line(&compound->lines);
-	free_spline(&compound->splines);
-	free_text(&compound->texts);
-	if (compound->comments) {
-	    free(compound->comments);
-	    compound->comments = NULL;
+	for (a = *list; a != NULL;) {
+		arc = a;
+		a = a->next;
+		if (arc->for_arrow)
+			free((char *) arc->for_arrow);
+		if (arc->back_arrow)
+			free((char *) arc->back_arrow);
+		if (arc->comments)
+			free(arc->comments);
+		free((char *) arc);
 	}
-	free((char *) compound);
-	compound = NULL;
-    }
-    *list = NULL;
+	*list = NULL;
 }
 
-void free_ellipse(F_ellipse **list)
+void
+free_compound(F_compound **list)
 {
-    F_ellipse	   *e, *ellipse;
+	F_compound	   *c, *compound;
 
-    for (e = *list; e != NULL;) {
-	ellipse = e;
-	e = e->next;
-	if (ellipse->comments)
-	    free(ellipse->comments);
-	free((char *) ellipse);
-    }
-    *list = NULL;
+	for (c = *list; c != NULL;) {
+		compound = c;
+		c = c->next;
+		free_arc(&compound->arcs);
+		free_compound(&compound->compounds);
+		free_ellipse(&compound->ellipses);
+		free_line(&compound->lines);
+		free_spline(&compound->splines);
+		free_text(&compound->texts);
+		if (compound->comments) {
+			free(compound->comments);
+			compound->comments = NULL;
+		}
+		free((char *) compound);
+		compound = NULL;
+	}
+	*list = NULL;
 }
 
-void free_line(F_line **list)
+void
+free_ellipse(F_ellipse **list)
 {
-    F_line	   *l, *line;
+	F_ellipse	   *e, *ellipse;
 
-    for (l = *list; l != NULL;) {
-	line = l;
-	l = l->next;
-	free_linestorage(line);
-    }
-    *list = NULL;
+	for (e = *list; e != NULL;) {
+		ellipse = e;
+		e = e->next;
+		if (ellipse->comments)
+			free(ellipse->comments);
+		free((char *) ellipse);
+	}
+	*list = NULL;
 }
 
-void free_text(F_text **list)
+void
+free_line(F_line **list)
 {
-    F_text	   *t, *text;
+	F_line	   *l, *line;
 
-    for (t = *list; t != NULL;) {
-	text = t;
-	t = t->next;
-	free(text->cstring);
-	if (text->xftfont)
-		closefont(text->xftfont);
-	if (text->comments)
-	    free(text->comments);
-	free(text);
-    }
-    *list = NULL;
+	for (l = *list; l != NULL;) {
+		line = l;
+		l = l->next;
+		free_linestorage(line);
+	}
+	*list = NULL;
 }
 
-void free_spline(F_spline **list)
+void
+free_text(F_text **list)
 {
-    F_spline	   *s, *spline;
+	F_text	   *t, *text;
 
-    for (s = *list; s != NULL;) {
-	spline = s;
-	s = s->next;
-	free_splinestorage(spline);
-    }
-    *list = NULL;
+	for (t = *list; t != NULL;) {
+		text = t;
+		t = t->next;
+		free(text->cstring);
+		if (text->xftfont)
+			closefont(text->xftfont);
+		if (text->comments)
+			free(text->comments);
+		free(text);
+	}
+	*list = NULL;
 }
 
-void free_splinestorage(F_spline *s)
+void
+free_spline(F_spline **list)
 {
+	F_spline	   *s, *spline;
 
-    free_points(s->points);
-    free_sfactors(s->sfactors);
-    if (s->for_arrow)
-	free((char *) s->for_arrow);
-    if (s->back_arrow)
-	free((char *) s->back_arrow);
-    if (s->comments)
-	free(s->comments);
-    free((char *) s);
+	for (s = *list; s != NULL;) {
+		spline = s;
+		s = s->next;
+		free_splinestorage(spline);
+	}
+	*list = NULL;
 }
 
-void free_linestorage(F_line *l)
+void
+free_splinestorage(F_spline *s)
 {
-    free_points(l->points);
-    if (l->for_arrow)
-	free((char *) l->for_arrow);
-    if (l->back_arrow)
-	free((char *) l->back_arrow);
-    if (l->pic) {
-	free_picture_entry(l->pic->pic_cache);
-	if (l->pic->pixmap != 0)
-	    XFreePixmap(tool_d, l->pic->pixmap);
-	l->pic->pixmap = (Pixmap) 0;
-	if (l->pic->mask != 0)
-	    XFreePixmap(tool_d, l->pic->mask);
-	l->pic->mask = (Pixmap) 0;
-	free((char *) l->pic);
-    }
-    if (l->comments)
-	free(l->comments);
-    free((char *) l);
+	free_points(s->points);
+	free_sfactors(s->sfactors);
+	if (s->for_arrow)
+		free((char *) s->for_arrow);
+	if (s->back_arrow)
+		free((char *) s->back_arrow);
+	if (s->comments)
+		free(s->comments);
+	free((char *) s);
 }
 
-void free_picture_entry(struct _pics *picture)
+void
+free_linestorage(F_line *l)
 {
-    if (!picture)
-	return;
-
-    if (picture->refcount == 0) {
-	fprintf(stderr, "Error freeing picture %p %s with refcount = 0\n",
-			(void *)picture, picture->file);
-	return;
-    }
-    picture->refcount--;
-    if (picture->refcount == 0) {
-	if (appres.DEBUG)
-	    fprintf(stderr,"Delete picture %p %s, refcount = %d\n",
-			    (void *)picture, picture->file, picture->refcount);
-	if (picture->bitmap)
-	    free((char *) picture->bitmap);
-	free(picture->file);
-	/* unlink from list */
-	if (picture->next)
-	    picture->next->prev = picture->prev;
-	if (picture->prev)
-	    picture->prev->next = picture->next;
-	/* at the head of the list */
-	if (picture->prev == NULL)
-	    pictures = NULL;
-	free(picture);
-    } else {
-	if (appres.DEBUG)
-	    fprintf(stderr,
-			"Decrease refcount for picture %p %s, refcount = %d\n",
-			(void *)picture, picture->file, picture->refcount);
-    }
+	free_points(l->points);
+	if (l->for_arrow)
+		free((char *) l->for_arrow);
+	if (l->back_arrow)
+		free((char *) l->back_arrow);
+	if (l->pic) {
+		free_picture_entry(l->pic->pic_cache);
+		if (l->pic->pixmap != 0)
+			XFreePixmap(tool_d, l->pic->pixmap);
+		l->pic->pixmap = (Pixmap) 0;
+		if (l->pic->mask != 0)
+			XFreePixmap(tool_d, l->pic->mask);
+		l->pic->mask = (Pixmap) 0;
+		free((char *) l->pic);
+	}
+	if (l->comments)
+		free(l->comments);
+	free((char *) l);
 }
 
-void free_points(F_point *first_point)
+void
+free_picture_entry(struct _pics *picture)
 {
-    F_point	   *p, *q;
+	if (!picture)
+		return;
 
-    for (p = first_point; p != NULL; p = q) {
-	q = p->next;
-	free((char *) p);
-    }
+	if (picture->refcount == 0) {
+		fprintf(stderr, "Error freeing picture %p %s with refcount = 0\n",
+				(void *)picture, picture->file);
+		return;
+	}
+	picture->refcount--;
+	if (picture->refcount == 0) {
+		if (appres.DEBUG)
+			fprintf(stderr, "Delete picture %p %s, refcount = %d\n",
+					(void *)picture, picture->file,
+					picture->refcount);
+		if (picture->bitmap)
+			free(picture->bitmap);
+		free(picture->file);
+		/* unlink from list */
+		if (picture->next)
+			picture->next->prev = picture->prev;
+		if (picture->prev)
+			picture->prev->next = picture->next;
+		/* at the head of the list */
+		if (picture->prev == NULL)
+			pictures = NULL;
+		free(picture);
+	} else {
+		if (appres.DEBUG)
+			fprintf(stderr,
+					"Decrease refcount for picture %p %s, refcount = %d\n",
+					(void *)picture, picture->file,
+					picture->refcount);
+	}
 }
 
-void free_sfactors(F_sfactor *sf)
+void
+free_points(F_point *first_point)
 {
-    F_sfactor	   *a, *b;
-    for (a = sf; a != NULL; a = b) {
-	b = a->next;
-	free((char *) a);
-    }
+	F_point	   *p, *q;
+
+	for (p = first_point; p != NULL; p = q) {
+		q = p->next;
+		free((char *) p);
+	}
 }
 
-void free_linkinfo(F_linkinfo **list)
+void
+free_sfactors(F_sfactor *sf)
 {
-    F_linkinfo	   *l, *link;
+	F_sfactor	   *a, *b;
+	for (a = sf; a != NULL; a = b) {
+		b = a->next;
+		free((char *) a);
+	}
+}
 
-    for (l = *list; l != NULL;) {
-	link = l;
-	l = l->next;
-	free((char *) link);
-    }
-    *list = NULL;
+void
+free_linkinfo(F_linkinfo **list)
+{
+	F_linkinfo	   *l, *link;
+
+	for (l = *list; l != NULL;) {
+		link = l;
+		l = l->next;
+		free((char *) link);
+	}
+	*list = NULL;
 }
 
 /* free up all the GC's before leaving xfig */
 
-void free_GCs(void)
+void
+free_GCs(void)
 {
 #ifdef USE_XPM
 	/* free any colors from the xfig xpm icon (if used) */
 	if (xfig_icon_attr.npixels > 0) {
-	    XFreeColors(tool_d, tool_cm,
-			xfig_icon_attr.pixels, xfig_icon_attr.npixels,
-			(unsigned long) 0);
+		XFreeColors(tool_d, tool_cm,
+				xfig_icon_attr.pixels, xfig_icon_attr.npixels,
+				(unsigned long) 0);
 	}
 #endif /* USE_XPM */
 	XFreeGC(tool_d, border_gc);
@@ -261,16 +275,17 @@ void free_GCs(void)
 }
 /* free up all the Fonts before leaving xfig */
 
-void free_Fonts(void)
+void
+free_Fonts(void)
 {
-  int i;
+	int i;
 
-  if (roman_font!=NULL) {
-    XFreeFont(tool_d, roman_font);
-  };
-  if (button_font!=NULL) {
-    XFreeFont(tool_d, button_font);
-  };
+	if (roman_font!=NULL) {
+		XFreeFont(tool_d, roman_font);
+	};
+	if (button_font!=NULL) {
+		XFreeFont(tool_d, button_font);
+	};
 	XFreeGC(tool_d, sr_xor_gc);
 
 	for (i=0; i<NUMOPS; i++) {
