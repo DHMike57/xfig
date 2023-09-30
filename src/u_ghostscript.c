67744e128b19c5afbd8a8337907e8d1700c86dff
Thomas Loimer <thomas.loimer@tuwien.ac.at>
2023-09-30 21:04:42 +0200

    Fortify stdout_mediabox()

--- src/u_ghostscript.c
+++ src/u_ghostscript.c
@@ -3,7 +3,7 @@
  * Copyright (c) 1985-1988 by Supoj Sutanthavibul
  * Parts Copyright (c) 1989-2015 by Brian V. Smith
  * Parts Copyright (c) 1991 by Paul King
- * Parts Copyright (c) 2016-2022 by Thomas Loimer
+ * Parts Copyright (c) 2016-2023 by Thomas Loimer
  *
  * Any party obtaining a copy of these files is granted, free of charge, a
  * full and unrestricted irrevocable, world-wide, paid up, royalty-free,
@@ -115,8 +115,17 @@ stdout_mediabox(void *caller_handle, const char *str, int len)
 	int	err;
 	double	fbb[4];
 
-	/* This rests on the assumption that ghostscript writes the required
-	   information all at once to str. Should use a buffer, instead. */
+	if (data->bb[2] >= 0 || data->bb[2] == GS_ERROR) {
+		/* bounding box already determined */
+		return len;
+	} else if (!strchr(str, ']')) {
+		/* return a short character read for incomplete information */
+		char	*p;
+		if ((p = strchr(str, '[')))
+			return p - str;
+		else
+			return 0;
+	}
 
 	/* gs always uses full stops as decimal character. The locale is already
 	   set to C in read_pdf() */
